0ae7e264b3c07c8deabdfd720ecdcda205c6bf59
Roman Mukovnin <r.mykovnin@gmail.com>
2022-05-11 21:00:44 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice;
+        ticketPrice = 450;
+        int aboutOneMile;
+        aboutOneMile = 20;
+        int totalMiles = ticketPrice / aboutOneMile;
+        System.out.println(" Ваших бонусных миль = " + totalMiles);
+    }
+}
